4d070e43fd5f1452dedcf0004d8942bab6abf148
Ben Carman <benthecarman@live.com>
2020-07-05 22:56:35 -0500

    Better error handling for ExtractTransactionDialog

--- src/main/scala/org/psbttoolkit/gui/dialog/ExtractTransactionDialog.scala
+++ src/main/scala/org/psbttoolkit/gui/dialog/ExtractTransactionDialog.scala
@@ -3,7 +3,8 @@ package org.psbttoolkit.gui.dialog
 import org.bitcoins.core.protocol.transaction.Transaction
 import org.psbttoolkit.gui.GlobalData
 import scalafx.Includes._
-import scalafx.scene.control.{ButtonType, Dialog, Label, TextArea}
+import scalafx.scene.control.Alert.AlertType
+import scalafx.scene.control.{Alert, ButtonType, Dialog, TextArea}
 import scalafx.scene.layout.VBox
 import scalafx.stage.Window
 
@@ -13,27 +14,33 @@ object ExtractTransactionDialog {
       parentWindow: Window,
       transaction: Transaction,
       errOpt: Option[Throwable]): Unit = {
-    val dialog = new Dialog[Unit]() {
-      initOwner(parentWindow)
-      title = "Extract Transaction"
-    }
-
-    dialog.dialogPane().buttonTypes = Seq(ButtonType.OK)
-    dialog.dialogPane().stylesheets = GlobalData.currentStyleSheets
-
-    val labelOpt = errOpt.map(err =>
-      new Label(
-        s"Warning! This transaction does not appear to be valid, error ${err.getMessage}"))
 
     val rawTxTA = new TextArea {
       text = transaction.hex
+      wrapText = true
       editable = false
     }
 
-    dialog.dialogPane().content = new VBox {
-      children = Vector(labelOpt, Some(rawTxTA)).flatten
+    val _ = errOpt match {
+      case Some(err) =>
+        new Alert(AlertType.Warning) {
+          initOwner(owner)
+          title = "Extract Transaction"
+          headerText =
+            s"Warning! This transaction does not appear to be valid, error ${err.getMessage}"
+          dialogPane().content = rawTxTA
+          dialogPane().stylesheets = GlobalData.currentStyleSheets
+        }.showAndWait()
+      case None =>
+        new Dialog[Unit]() {
+          initOwner(parentWindow)
+          title = "Extract Transaction"
+          dialogPane().buttonTypes = Seq(ButtonType.OK)
+          dialogPane().stylesheets = GlobalData.currentStyleSheets
+          dialogPane().content = new VBox {
+            children = rawTxTA
+          }
+        }.showAndWait()
     }
-
-    val _ = dialog.showAndWait()
   }
 }
