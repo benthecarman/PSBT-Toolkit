d7795b024e2b8038c9bccfd0a84b9b05d4896c39
Ben Carman <benthecarman@live.com>
2020-07-05 23:07:35 -0500

    Center items in Gen Pane

--- src/main/scala/org/psbttoolkit/gui/generators/GeneratorsPane.scala
+++ src/main/scala/org/psbttoolkit/gui/generators/GeneratorsPane.scala
@@ -1,8 +1,8 @@
 package org.psbttoolkit.gui.generators
 
 import org.psbttoolkit.gui.TaskRunner
-import scalafx.geometry.Orientation
-import scalafx.scene.control.{Separator, TextArea}
+import scalafx.geometry.{Orientation, Pos}
+import scalafx.scene.control.{Label, Separator, TextArea}
 import scalafx.scene.layout.{BorderPane, HBox, VBox}
 
 class GeneratorsPane(glassPane: VBox) {
@@ -18,13 +18,27 @@ class GeneratorsPane(glassPane: VBox) {
 
   private val genButtons = new GeneratorButtons(model)
 
-  private val txSide = new HBox {
+  private val txButton = new HBox {
     children = List(genButtons.txGenSelector, genButtons.txGenButton)
     spacing = 10
   }
 
-  private val psbtSide = new HBox {
+  private val txSide = new VBox {
+    alignment = Pos.TopCenter
+    children =
+      Vector(new Label("Transaction Generators"), new Separator, txButton)
+    spacing = 10
+  }
+
+  private val psbtButton = new HBox {
+    alignment = Pos.TopCenter
     children = List(genButtons.psbtGenSelector, genButtons.psbtGenButton)
+    spacing = 10
+  }
+
+  private val psbtSide = new VBox {
+    alignment = Pos.TopCenter
+    children = Vector(new Label("PSBT Generators"), new Separator, psbtButton)
     spacing = 10
   }
 
@@ -33,6 +47,7 @@ class GeneratorsPane(glassPane: VBox) {
   }
 
   private val buttonPane = new HBox {
+    alignment = Pos.TopCenter
     children = List(txSide, sep, psbtSide)
     spacing = 10
   }
