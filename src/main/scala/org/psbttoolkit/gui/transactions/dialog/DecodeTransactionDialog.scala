241cee7518b9d4f2c908c11a00d690802ef46115
Ben Carman <benthecarman@live.com>
2020-08-15 02:39:59 -0500

    Fix Decode Tx

--- src/main/scala/org/psbttoolkit/gui/transactions/dialog/DecodeTransactionDialog.scala
+++ src/main/scala/org/psbttoolkit/gui/transactions/dialog/DecodeTransactionDialog.scala
@@ -2,7 +2,7 @@ package org.psbttoolkit.gui.transactions.dialog
 
 import org.psbttoolkit.gui.GlobalData
 import scalafx.Includes.jfxDialogPane2sfx
-import scalafx.scene.control.{ButtonType, Dialog, TextArea}
+import scalafx.scene.control.{ButtonType, Dialog, Label}
 import scalafx.stage.Window
 
 object DecodeTransactionDialog {
@@ -14,13 +14,11 @@ object DecodeTransactionDialog {
       title = "Decoded Transaction"
       dialogPane().buttonTypes = Seq(ButtonType.OK)
       dialogPane().stylesheets = GlobalData.currentStyleSheets
-      // todo dynamically size the text area
-      dialogPane().content = new TextArea {
-        text = decoded
-        editable = false
+      dialogPane().content = new Label(decoded) {
         maxHeight = Double.MaxValue
         maxWidth = Double.MaxValue
       }
+      dialogPane().getScene.getWindow.sizeToScene()
     }.showAndWait()
   }
 }
